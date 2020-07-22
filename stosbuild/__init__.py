73d408fa4404e9284ddb4afa404bcee14290a17b
spamandeggs <jerome.mahieux.web@gmail.com>
2020-07-22 19:22:17 +0200

    initial commit

---
+++ stosbuild/__init__.py
@@ -0,0 +1,5 @@
+# incoming python 3 module to regenerate STOS Atari project from pieces
+# 2020/07/22
+
+def void() :
+    pass
---
+++ stosbuild/readme.txt
@@ -0,0 +1,2 @@
+Incoming python 3 module to regenerate STOS Atari project from pieces.
+2020/07/22
